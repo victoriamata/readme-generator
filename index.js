d5348cc4c7b6080c3ac2a7a35e27cf1c480866ed
victoriamata <victoriamataxo@gmail.com>
2024-10-09 15:55:34 -0400

    Added starter code to JS file

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 import fs from 'fs';
 
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
