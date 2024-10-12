e6ee2efea887693dee37c92ceeb75cdcde5cfc3b
victoriamata <victoriamataxo@gmail.com>
2024-10-11 20:35:40 -0400

    Corrected array of questions reflecting the requirements

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 # README Generator 
-
-  ## Description:
-
-  ## Table of Contents:
-
-  ## Installation:
-
-  ## Usage:
-
-  ## License: 
-
-  ## Contributing:
-
-  ## Tests:
-
-  ## Questions:
+   ReadMe Generator
+   ## Description:
+   Testing my skills
+   ## Table of Contents:
+   * [Installation](#installation)
+   * [Usage](#usage)
+   * [License](#license)
+   * [Contributing](#contributing)
+   * [Tests](#tests)
+   * [Questions](#questions)
+   ## Installation:
+   Installing it
+   ## Usage:
+   Use it
+   ## License: 
+   MIT
+   ## Contributing:
+   ME
+   ## Tests:
+   test it
+   ## Questions:
+   * Github: [Click this link!](https://github.com/victoriamata)
+   * Email: victoriamataxo@gmail.com
--- index.js
+++ index.js
@@ -2,57 +2,89 @@
 import fs from 'fs';
 import inquirer from 'inquirer';
 
-const generate = ({ name, location, github, linkedin }) =>
+const generate = ({title, description, installation, usage, contributing, tests, license, github, email}) =>
   `# README Generator 
-
-  ## Description:
-
-  ## Table of Contents:
-
-  ## Installation:
-
-  ## Usage:
-
-  ## License: 
-
-  ## Contributing:
-
-  ## Tests:
-
-  ## Questions:`;
+   ${title}
+   ## Description:
+   ${description}
+   ## Table of Contents:
+   * [Installation](#installation)
+   * [Usage](#usage)
+   * [License](#license)
+   * [Contributing](#contributing)
+   * [Tests](#tests)
+   * [Questions](#questions)
+   ## Installation:
+   ${installation}
+   ## Usage:
+   ${usage}
+   ## License: 
+   ${license}
+   ## Contributing:
+   ${contributing}
+   ## Tests:
+   ${tests}
+   ## Questions:
+   * Github: [Click this link!](https://github.com/${github})
+   * Email: ${email}`;
 
 // TODO: Create an array of questions for user input
 inquirer
   .prompt([
     {
       type: "input",
-      message: "What is your name?",
-      name: "nameQuestion"
+      message: "Please enter project title:",
+      name: "title"
     }, {
       type: "input",
-      message: "What languages do you know?",
-      name: "languages"
+      message: "What is the description?",
+      name: "description"
+    }, {
+      type: "input",
+      message: "Please enter the installation instructions:",
+      name: "installation"
+    }, {
+      type: "input",
+      message: "Please enter the usage information:",
+      name: "usage"
+    }, {
+      type: "input",
+      message: "Who contributed to this project?",
+      name: "contributing"
+    }, {
+      type: "input",
+      message: "What are the testing intructions?",
+      name: "tests"
     }, {
       type: "list",
-      message: "What is your preferred method of communication",
-      name: "communication",
-      choices: ["Text","Email","Call"]
+      message: "Please choose a license:",
+      name: "license",
+      choices: ["MIT","Mozilla","Apache"]
+    }, 
+    {
+      type: "input",
+      message: "What is your Github username?:",
+      name: "github"
+    }, {
+      type: "input",
+      message: "Please enter your email:",
+      name: "email"
     }
   ])
   .then((answers) => {
-    const htmlPageContent = generate(answers);
+    const readmeContent = generate(answers);
 
 // TODO: Create a function to write README file
 // seems like it should call fs.writeFile
-// function writeToFile(fileName, data) {
+function writeToFile(fileName, data) {}
 
-fs.writeFile('README.md', htmlPageContent, (err) =>
-  err ? console.log(err) : console.log('Successfully created readme!')
+fs.writeFile('README.md', readmeContent, (err) =>
+  err ? console.log(err) : console.log('Successfully created a README file!')
 );
 });
 // TODO: Create a function to initialize app
 // this will probably call inquirer.prompt(...).then(...) 
-// function init() {}
+function init() {}
 
 // Function call to initialize app
-// init();
+init();
